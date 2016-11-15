287c2ddd1f65d84fc53891c46550398a1c6097df
Fan Jin <fjinss@gmail.com>
2016-11-15 12:36:33 +1100

    init

--- app/app.js
+++ app/app.js
@@ -3,12 +3,21 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
+  'myApp.work',
+  'myApp.research',
   'myApp.version'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/work'});
+}]).
+controller('FanJinCtrl', ['$scope', '$element', '$location', function($scope, $event, $location){
+	var self = $scope;
+	self.location = $location;
+
+	$scope.changeView = function(path,$event){
+		$($event.currentTarget.parentElement).siblings().toggleClass("active");
+		$($event.currentTarget.parentElement).toggleClass("active");
+	}
 }]);
---
+++ app/research/research_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('myApp.research module', function() {
+
+  beforeEach(module('myApp.research'));
+
+  describe('research controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var researchCtrl = $controller('ResearchCtrl');
+      expect(researchCtrl).toBeDefined();
+    }));
+
+  });
+});
